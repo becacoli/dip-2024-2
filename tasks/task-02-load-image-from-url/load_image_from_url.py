46c37ab533b815f5e34add568d3e421020936f97
Rebeca Queiroz <rebeca.correia100@gmail.com>
2025-02-23 11:56:38 -0300

    task 02 resolved

--- tasks/task-02-load-image-from-url/load_image_from_url.py
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import cv2 as cv
+import requests
 
 def load_image_from_url(url, **kwargs):
     """
@@ -15,9 +16,14 @@ def load_image_from_url(url, **kwargs):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    response_url = requests.get(url)
+    response_url.raise_for_status()
+    image_array = np.asarray(bytearray(response_url.content), dtype=np.uint8)
+
+    image = cv.imdecode(image_array, cv.IMREAD_COLOR)  
+    cv.imwrite("image.jpg", image)
     ### END CODE HERE ###
     
     return image
 
-load_image_from_url()
+load_image_from_url("https://www.kalmbachfeeds.com/cdn/shop/articles/person-holding-two-chinchillas-in-their-arms.jpg?v=1706873615")
