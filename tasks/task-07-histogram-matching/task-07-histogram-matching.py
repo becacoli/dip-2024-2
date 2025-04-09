439b0b7d78ecf4da641df6773a75720186679fe8
Rebeca Queiroz <rebeca.correia100@gmail.com>
2025-04-09 06:09:24 -0300

    task 07 done

--- tasks/task-07-histogram-matching/task-07-histogram-matching.py
+++ tasks/task-07-histogram-matching/task-07-histogram-matching.py
@@ -26,8 +26,38 @@ Notes:
 
 import cv2 as cv
 import numpy as np
-import scikitimage as ski
 
 def match_histograms_rgb(source_img: np.ndarray, reference_img: np.ndarray) -> np.ndarray:
-    # Your implementation here
-    pass
+    
+    matched_img = np.zeros_like(source_img)
+    
+    for channel in range(3):
+        
+        src_channel = source_img[:, :, channel]
+        ref_channel = reference_img[:, :, channel]
+        
+        src_hist, bins = np.histogram(src_channel.ravel(), 256, [0, 256])
+        ref_hist, _ = np.histogram(ref_channel.ravel(), 256, [0, 256])
+        
+        src_cdf = np.cumsum(src_hist)
+        ref_cdf = np.cumsum(ref_hist)
+        
+        src_cdf_normalized = src_cdf / src_cdf[-1]
+        ref_cdf_normalized = ref_cdf / ref_cdf[-1]
+        interp_map = np.zeros(256)
+        
+        j = 0
+        for i in range(256):
+            while j < 255 and ref_cdf_normalized[j] < src_cdf_normalized[i]:
+                j += 1
+            if j > 0 and j < 255:
+                f = (src_cdf_normalized[i] - ref_cdf_normalized[j-1]) / (ref_cdf_normalized[j] - ref_cdf_normalized[j-1])
+                interp_map[i] = (j-1) + f
+            else:
+                interp_map[i] = j
+        matched_img[:, :, channel] = interp_map[src_channel]
+        
+    matched_img = np.clip(matched_img, 0, 255)
+    
+    return matched_img.astype(np.uint8)
+
